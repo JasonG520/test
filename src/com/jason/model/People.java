3e59060995f8ff0154c880447c49160b6baf637b
JasonG520 <45389404+JasonG520@users.noreply.github.com>
2022-03-19 19:42:55 +0800

    Update People.java

--- src/com/jason/model/People.java
+++ src/com/jason/model/People.java
@@ -5,46 +5,5 @@ public class People {
     private Integer id;
     private String name;
     private String useranme;
-    private String password;
-    private String address;
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getUseranme() {
-        return useranme;
-    }
-
-    public void setUseranme(String useranme) {
-        this.useranme = useranme;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+   
 }
