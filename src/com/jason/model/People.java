3086ac657f852946513047221cdf5578ac3c40c0
jason <1299484641@qq.com>
2022-03-19 19:26:08 +0800

    add password

--- src/com/jason/model/People.java
+++ src/com/jason/model/People.java
@@ -4,6 +4,15 @@ public class People {
 
     private Integer id;
     private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public Integer getId() {
         return id;
